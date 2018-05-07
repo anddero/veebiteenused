5361861b3f45b736edad7b1005014f21224f310a
Karl-Andero Mere <anddero@gmail.com>
2018-05-07 17:04:43 +0300

    Initial java implementation

--- MusicWebApplication/src/java/ee/ttu/idu0075/music/MusicWebService.java
+++ MusicWebApplication/src/java/ee/ttu/idu0075/music/MusicWebService.java
@@ -5,53 +5,183 @@
  */
 package ee.ttu.idu0075.music;
 
+import ee.ttu.idu0075._2018.ws._164718.music.AddDiskRequest;
+import ee.ttu.idu0075._2018.ws._164718.music.AddDiskSongRequest;
+import ee.ttu.idu0075._2018.ws._164718.music.AddSongRequest;
+import ee.ttu.idu0075._2018.ws._164718.music.DiskSongListType;
+import ee.ttu.idu0075._2018.ws._164718.music.DiskSongType;
+import ee.ttu.idu0075._2018.ws._164718.music.DiskType;
+import ee.ttu.idu0075._2018.ws._164718.music.GetDiskListRequest;
+import ee.ttu.idu0075._2018.ws._164718.music.GetDiskListResponse;
+import ee.ttu.idu0075._2018.ws._164718.music.GetDiskRequest;
+import ee.ttu.idu0075._2018.ws._164718.music.GetDiskSongListRequest;
+import ee.ttu.idu0075._2018.ws._164718.music.GetSongListRequest;
+import ee.ttu.idu0075._2018.ws._164718.music.GetSongListResponse;
+import ee.ttu.idu0075._2018.ws._164718.music.GetSongRequest;
+import ee.ttu.idu0075._2018.ws._164718.music.SongType;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.jws.WebService;
 
 /**
  *
  * @author Karl-Andero Mere
  */
-@WebService(serviceName = "MusicService", portName = "MusicPort", endpointInterface = "ee.ttu.idu0075._2018.ws._164718.music.MusicPortType", targetNamespace = "http://www.ttu.ee/idu0075/2018/ws/164718/music", wsdlLocation = "WEB-INF/wsdl/MusicWebService/MusicService.wsdl")
+@WebService(serviceName = "MusicService",
+        portName = "MusicPort",
+        endpointInterface = "ee.ttu.idu0075._2018.ws._164718.music.MusicPortType",
+        targetNamespace = "http://www.ttu.ee/idu0075/2018/ws/164718/music",
+        wsdlLocation = "WEB-INF/wsdl/MusicWebService/MusicService.wsdl")
 public class MusicWebService {
+    private static final String PASSWORD = "salajane";
+    private static final List<SongType> songs = new ArrayList<>();
+    private static final List<DiskType> disks = new ArrayList<>();
+    private static final HashMap<BigInteger, SongType> addSongRequests = new HashMap<>();
+    private static final HashMap<BigInteger, DiskType> addDiskRequests = new HashMap<>();
+    private static final HashMap<BigInteger, DiskSongType> addDiskSongRequests = new HashMap<>();
+    private static BigInteger autoId = BigInteger.ZERO;
 
-    public ee.ttu.idu0075._2018.ws._164718.music.SongType getSong(ee.ttu.idu0075._2018.ws._164718.music.GetSongRequest parameter) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public SongType getSong(GetSongRequest getSongRequest) {
+        if (!PASSWORD.equalsIgnoreCase(getSongRequest.getToken())) {
+            return null;
+        }
+        
+        return songs.stream()
+                .filter(s -> getSongRequest.getId().equals(s.getId()))
+                .findAny()
+                .orElse(null);
     }
 
-    public ee.ttu.idu0075._2018.ws._164718.music.SongType addSong(ee.ttu.idu0075._2018.ws._164718.music.AddSongRequest parameter) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public SongType addSong(AddSongRequest addSongRequest) {
+        if (!PASSWORD.equalsIgnoreCase(addSongRequest.getToken())) {
+            return null;
+        }
+        
+        if (!addSongRequests.containsKey(addSongRequest.getRequestCode())) {
+            SongType newSong = new SongType();
+            newSong.setId(autoId);
+            newSong.setName(addSongRequest.getName());
+            newSong.setAuthor(addSongRequest.getAuthor());
+            newSong.setLengthSeconds(addSongRequest.getLengthSeconds());
+            songs.add(newSong);
+            addSongRequests.put(addSongRequest.getRequestCode(), newSong);
+            autoId = autoId.add(BigInteger.ONE);
+        }
+        
+        return addSongRequests.get(addSongRequest.getRequestCode());
     }
 
-    public ee.ttu.idu0075._2018.ws._164718.music.GetSongListResponse getSongList(ee.ttu.idu0075._2018.ws._164718.music.GetSongListRequest parameter) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public GetSongListResponse getSongList(GetSongListRequest getSongListRequest) {
+        if (!PASSWORD.equalsIgnoreCase(getSongListRequest.getToken())) {
+            return null;
+        }
+        
+        GetSongListResponse response = new GetSongListResponse();
+        response.getSong().addAll(songs);
+        return response;
     }
 
-    public ee.ttu.idu0075._2018.ws._164718.music.DiskType getDisk(ee.ttu.idu0075._2018.ws._164718.music.GetDiskRequest parameter) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public DiskType getDisk(GetDiskRequest getDiskRequest) {
+        if (!PASSWORD.equalsIgnoreCase(getDiskRequest.getToken())) {
+            return null;
+        }
+        
+        return disks.stream()
+                .filter(disk -> getDiskRequest.getId().equals(disk.getId()))
+                .findAny()
+                .orElse(null);
     }
 
-    public ee.ttu.idu0075._2018.ws._164718.music.DiskType addDisk(ee.ttu.idu0075._2018.ws._164718.music.AddDiskRequest parameter) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public DiskType addDisk(AddDiskRequest addDiskRequest) {
+        if (!PASSWORD.equalsIgnoreCase(addDiskRequest.getToken())) {
+            return null;
+        }
+        
+        if (!addDiskRequests.containsKey(addDiskRequest.getRequestCode())) {
+            DiskType newDisk = new DiskType();
+            newDisk.setId(autoId);
+            newDisk.setName(addDiskRequest.getName());
+            newDisk.setAuthor(addDiskRequest.getAuthor());
+            newDisk.setDiskSongList(new DiskSongListType());
+            newDisk.setLengthSeconds(BigInteger.ZERO);
+            disks.add(newDisk);
+            addDiskRequests.put(addDiskRequest.getRequestCode(), newDisk);
+            autoId = autoId.add(BigInteger.ONE);
+        }
+        
+        return addDiskRequests.get(addDiskRequest.getRequestCode());
     }
 
-    public ee.ttu.idu0075._2018.ws._164718.music.GetDiskListResponse getDiskList(ee.ttu.idu0075._2018.ws._164718.music.GetDiskListRequest parameter) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public GetDiskListResponse getDiskList(GetDiskListRequest request) {
+        if (!PASSWORD.equalsIgnoreCase(request.getToken())) {
+            return null;
+        }
+        
+        GetDiskListResponse response = new GetDiskListResponse();
+        response.getDisk().addAll(disks.stream()
+                .filter(disk -> request.getMaxLengthSeconds() == null
+                        || request.getMaxLengthSeconds().compareTo(disk.getLengthSeconds()) >= 0)
+                .filter(disk -> request.getMinLengthSeconds() == null
+                        || request.getMinLengthSeconds().compareTo(disk.getLengthSeconds()) <= 0)
+                .filter(disk -> request.getMaxSongCount() == null
+                        || request.getMaxSongCount().compareTo(BigInteger.valueOf(disk.getDiskSongList().getDiskSong().size())) >= 0)
+                .filter(disk -> request.getMinSongCount() == null
+                        || request.getMinSongCount().compareTo(BigInteger.valueOf(disk.getDiskSongList().getDiskSong().size())) <= 0)
+                .collect(Collectors.toList()));
+        return response;
     }
 
-    public ee.ttu.idu0075._2018.ws._164718.music.DiskSongListType getDiskSongList(ee.ttu.idu0075._2018.ws._164718.music.GetDiskSongListRequest parameter) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public DiskSongListType getDiskSongList(GetDiskSongListRequest getDiskSongListRequest) {
+        if (!PASSWORD.equalsIgnoreCase(getDiskSongListRequest.getToken())) {
+            return null;
+        }
+        
+        Optional<DiskType> optionalDisk =  disks.stream()
+                .filter(disk -> getDiskSongListRequest.getDiskId().equals(disk.getId()))
+                .findAny();
+        
+        if (optionalDisk.isPresent()) {
+            return optionalDisk.get().getDiskSongList();
+        }
+        
+        return null;
     }
 
-    public ee.ttu.idu0075._2018.ws._164718.music.DiskSongType addDiskSong(ee.ttu.idu0075._2018.ws._164718.music.AddDiskSongRequest parameter) {
-        //TODO implement this method
-        throw new UnsupportedOperationException("Not implemented yet.");
+    public DiskSongType addDiskSong(AddDiskSongRequest addDiskSongRequest) {
+        if (!PASSWORD.equalsIgnoreCase(addDiskSongRequest.getToken())) {
+            return null;
+        }
+        
+        if (!addDiskSongRequests.containsKey(addDiskSongRequest.getRequestCode())) {
+            Optional<DiskType> optionalDisk = disks.stream()
+                .filter(disk -> addDiskSongRequest.getDiskId().equals(disk.getId()))
+                .findAny();
+        
+            if (!optionalDisk.isPresent()) {
+                return null;
+            }
+            
+            Optional<SongType> optionalSong = songs.stream()
+                    .filter(song -> addDiskSongRequest.getSongId().equals(song.getId()))
+                    .findAny();
+            
+            if (!optionalSong.isPresent()) {
+                return null;
+            }
+            
+            DiskSongType newDiskSong = new DiskSongType();
+            newDiskSong.setSong(optionalSong.get());
+            newDiskSong.setFormat(addDiskSongRequest.getFormat());
+            newDiskSong.setStartTimeSeconds(addDiskSongRequest.getStartTimeSeconds());
+            optionalDisk.get().getDiskSongList().getDiskSong().add(newDiskSong);
+            addDiskSongRequests.put(addDiskSongRequest.getRequestCode(), newDiskSong);
+            autoId = autoId.add(BigInteger.ONE);
+        }
+        
+        return addDiskSongRequests.get(addDiskSongRequest.getRequestCode());
     }
-    
 }
